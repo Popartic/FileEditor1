419cd3fa76a1f1440ff961427ab83af1fc71fefe
User <mirkopop@gmail.com>
2022-10-31 20:07:58 +0100

    Finalna verzija

--- src/main/java/org/example/PredlogKorisnickogInterfejsa.java
+++ src/main/java/org/example/PredlogKorisnickogInterfejsa.java
@@ -1,17 +1,26 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class PredlogKorisnickogInterfejsa extends JDialog {
     private JPanel contentPane;
     private JButton buttonClose;
     private JButton buttonSave;
-    private JButton buttonOpen;
-    private JButton buttonGetSelection;
+    private JButton buttonOpenTop;
+    private JButton buttonOpenBottom;
+    private JButton buttonGetSelectionTop;
+    private JButton buttonGetSelectionBottom;
     private JTextArea textAreaTop;
     private JTextArea textAreaBottom;
     private JTextArea textAreaNew;
+    String directory;
+    String selection;
 
     public PredlogKorisnickogInterfejsa() {
         setContentPane(contentPane);
@@ -23,7 +32,29 @@ public class PredlogKorisnickogInterfejsa extends JDialog {
                 onButtonClose();
             }
         });
-
+        buttonSave.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onButtonSave();
+            }
+        });
+        buttonOpenTop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onButtonOpenTop();
+            }
+        });
+        buttonOpenBottom.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                                                   onButtonOpenBottom();
+                                               }
+        });
+        buttonGetSelectionTop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onButtonGetSelectionTop();
+            }
+        });
+        buttonGetSelectionBottom.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { onButtonGetSelectionBottom();}
+        });
 
         // call onCancel() when cross is clicked
         addWindowListener(new WindowAdapter() {
@@ -34,10 +65,146 @@ public class PredlogKorisnickogInterfejsa extends JDialog {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
 
+    private void onButtonGetSelectionTop() {
+        selection = textAreaTop.getSelectedText();
+        textAreaNew.append(selection);
+    }
+    private void onButtonGetSelectionBottom() {
+        selection = textAreaBottom.getSelectedText();
+        textAreaNew.append(selection);
+    }
+
+    private void onButtonOpenTop() {
+        // Create a file dialog box to prompt for a new file to display
+        FileDialog f = new FileDialog(this, "Otvori fajl", FileDialog.LOAD);
+        f.setDirectory(directory); // Set the default directory
+        // Display the dialog and wait for the user's response
+        f.setVisible(true);
+        directory = f.getDirectory(); // Remember new default directory
+        loadAndDisplayFile(directory, f.getFile()); // Load and display selection
+        f.dispose(); // Get rid of the dialog box
+    }
+    private void onButtonOpenBottom() {
+        // Create a file dialog box to prompt for a new file to display
+        FileDialog f = new FileDialog(this, "Otvori fajl", FileDialog.LOAD);
+        f.setDirectory(directory); // Set the default directory
+        // Display the dialog and wait for the user's response
+        f.setVisible(true);
+        directory = f.getDirectory(); // Remember new default directory
+        loadAndDisplayFile(directory, f.getFile()); // Load and display selection
+        f.dispose(); // Get rid of the dialog box
+    }
+
+    public void loadAndDisplayFile(String directory, String filename) {
+        if ((filename == null) || (filename.length() == 0))
+            return;
+        File file;
+        FileReader in = null;
+        // Read and display the file contents. Since we're reading text, we
+        // use a FileReader instead of a FileInputStream.
+        try {
+            file = new File(directory, filename); // Create a file object
+            in = new FileReader(file); // And a char stream to read it
+            char[] buffer = new char[4096]; // Read 4K characters at a time
+            int len; // How many chars read each time
+            textAreaTop.setText(""); // Clear the text area
+            while ((len = in.read(buffer)) != -1) { // Read a batch of chars
+                String s = new String(buffer, 0, len); // Convert to a string
+                textAreaTop.append(s); // And display them
+            }
+            this.setTitle("FileViewer: " + filename); // Set the window title
+            textAreaTop.setCaretPosition(0); // Go to start of file
+        }
+        // Display messages if something goes wrong
+        catch (IOException e) {
+            textAreaTop.setText(e.getClass().getName() + ": " + e.getMessage());
+            this.setTitle("FileViewer: " + filename + ": I/O Exception");
+        }
+        // Always be sure to close the input stream!
+        finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+    public void loadAndDisplayFile2(String directory, String filename) {
+        if ((filename == null) || (filename.length() == 0))
+            return;
+        File file;
+        FileReader in = null;
+        // Read and display the file contents. Since we're reading text, we
+        // use a FileReader instead of a FileInputStream.
+        try {
+            file = new File(directory, filename); // Create a file object
+            in = new FileReader(file); // And a char stream to read it
+            char[] buffer = new char[4096]; // Read 4K characters at a time
+            int len; // How many chars read each time
+            textAreaBottom.setText(""); // Clear the text area
+            while ((len = in.read(buffer)) != -1) { // Read a batch of chars
+                String s = new String(buffer, 0, len); // Convert to a string
+                textAreaBottom.append(s); // And display them
+            }
+            this.setTitle("FileViewer: " + filename); // Set the window title
+            textAreaBottom.setCaretPosition(0); // Go to start of file
+        }
+        // Display messages if something goes wrong
+        catch (IOException e) {
+            textAreaBottom.setText(e.getClass().getName() + ": " + e.getMessage());
+            this.setTitle("FileViewer: " + filename + ": I/O Exception");
+        }
+        // Always be sure to close the input stream!
+        finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+    public void saveFile(String directory, String filename) {
+        if ((filename == null) || (filename.length() == 0))
+            return;
+        File file;
+        FileWriter out = null;
+        try {
+            file = new File(directory, filename); // Create a file object
+            out = new FileWriter(file); // And a char stream to write it
+            textAreaNew.getLineCount(); // Get text from the text area
+            String s = textAreaNew.getText();
+            out.write(s);
+        }
+        // Display messages if something goes wrong
+        catch (IOException e) {
+            textAreaNew.setText(e.getClass().getName() + ": " + e.getMessage());
+            this.setTitle("FileViewer: " + filename + ": I/O Exception");
+        }
+        // Always be sure to close the input stream!
+        finally {
+            try {
+                if (out != null)
+                    out.close();
+            }
+            catch (IOException e) {
+            }
+        }
+    }
+    private void onButtonSave() { // Create a file dialog box to prompt for a new file to display
+        FileDialog f = new FileDialog(this, "Otvori fajl", FileDialog.SAVE);
+        f.setDirectory(directory); // Set the default directory
+        // Display the dialog and wait for the user's response
+        f.setVisible(true);
+        directory = f.getDirectory(); // Remember new default directory
+        saveFile(directory, f.getFile()); // Load and display selection
+        f.dispose(); // Get rid of the dialog box
+    }
+
     private void onButtonClose() {
         // add your code here if necessary
         dispose();
     }
+
     public static void main(String[] args) {
         PredlogKorisnickogInterfejsa dialog = new PredlogKorisnickogInterfejsa();
         dialog.pack();
